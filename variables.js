b89e14112d769a459e0495f042351809f88f7199
oscarleon <oscarleon.alcatel@gmai.com>
2024-11-07 19:19:06 -0600

    inicio del curso JS en Platzi

---
+++ variables.js
@@ -0,0 +1,17 @@
+let cajaDeOscar = 'Celular'
+console.log(cajaDeOscar)
+
+// No permitido
+
+let c = 'Telefono'
+let cda = 'Telefono'
+let pcAndy = 'Telefono'
+
+// Lo permitido
+let primeraCajaDeOscar = 'Telefonos'
+let urlDeUsuario = 'https://oscarleon.app'
+let idDeUsuario = '123456789'
+
+// Las variables siempre van al inicio
+// let se utiliza para valores que van a cambiar en el tiempo
+// const se utiliza para valores que nunca van a cambiar
