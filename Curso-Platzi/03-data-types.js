1fdcca0dd9b360d5aac9a1846343754d309c0cb3
oscarleon <oscarleon.alcatel@gmai.com>
2024-11-07 19:31:47 -0600

    tipo de datos

---
+++ Curso-Platzi/03-data-types.js
@@ -0,0 +1,36 @@
+// PRIMITIVOS
+
+// Strings
+let nombre = 'Oscar'
+
+// Number
+let edad = 34
+
+// Boolean
+let esMayorDeEdad = true
+
+// null si lo podemos agregar nosotros como programadores
+let noHayValor = null
+
+// undefined este no lo colocamos nostostros como programadores, lo hacer JS
+let noDefinido = undefined
+
+// Symbol si queremos que algo sea unico como un id de usuario
+let simboloUnico = Symbol('único')
+
+// Bigint para numeros muy grandes
+let numeroGrande = 2n
+
+// COMPLEJOS
+
+// Object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+// Array o Listas
+let frutas = ['manza', 'banano', 'uvas']
+
+// function nombre (que va hacer la función) {cuerpo de la función}
+function saludar (){}
